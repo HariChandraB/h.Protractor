2c3401a7442944c860aac4e0aa93ba7e605bdb18
harih <harih@HariHRPS2>
2021-02-05 00:04:32 -0600

    refracted

--- workspace-js/ProtractorTutorial/WindowHandles.js
+++ workspace-js/ProtractorTutorial/WindowHandles.js
@@ -1,105 +1,76 @@
 describe(
-		'Actions demo',
-		function() {
+	'Actions demo',
+	function() {
 
-			it(
-					'Open Posse website',
-					function() {
+		it(
+			'Open Posse website',
+			function() {
 
-						// moving the mouse into that textbox
-						// sendkeys
-						// keyboard arrow
-						// Keyboard enter
-						browser.get("https://posse.com/");
-						element(by.model("userInputQuery")).sendKeys("river");
-						browser.actions().mouseMove(
-								element(by.model("locationQuery")).sendKeys(
-										"london")).perform();
+				// moving the mouse into that textbox
+				// sendkeys
+				// keyboard arrow
+				// Keyboard enter
+				browser.get("https://posse.com/");
+				element(by.model("userInputQuery")).sendKeys("river");
+				browser.actions().mouseMove(element(by.model("locationQuery")).sendKeys("london")).perform();
+				browser.actions().sendKeys(protractor.Key.ARROW_DOWN).perform();
+				browser.actions().sendKeys(protractor.Key.ENTER).perform().then(function() {
+				browser.sleep(2000);
 
-						browser.actions().sendKeys(protractor.Key.ARROW_DOWN)
-								.perform();
-						browser
-								.actions()
-								.sendKeys(protractor.Key.ENTER)
-								.perform()
-								.then(
-										function() {
+				expect(element.all(by.css("div[ng-mouseover*='onSearchResultOver']")).count()).toEqual(7);
 
-											browser.sleep(2000);
+				element.all(by.css("div[ng-mouseover*='onSearchResultOver']")).get(0).click();
+				element(by.css("a[ng-href*='London/River Island']")).click().then(function() {browser.getTitle().then(function(
+													title) {
 
-											expect(
-													element
-															.all(
-																	by
-																			.css("div[ng-mouseover*='onSearchResultOver']"))
-															.count())
-													.toEqual(7);
+													console
+														.log(title
+															+ " title of the page before switching");
+												})
 
-											element
-													.all(
-															by
-																	.css("div[ng-mouseover*='onSearchResultOver']"))
-													.get(0).click();
-											element(
-													by
-															.css("a[ng-href*='London/River Island']"))
-													.click()
-													.then(
-															function() {
-																browser
-																		.getTitle()
-																		.then(
-																				function(
-																						title) {
+										browser
+											.getAllWindowHandles()
+											.then(
+												function(
+													handles) {
 
-																					console
-																							.log(title
-																									+ " title of the page before switching");
-																				})
+													browser
+														.switchTo()
+														.window(
+															handles[1]);
 
-																browser
-																		.getAllWindowHandles()
-																		.then(
-																				function(
-																						handles) {
+													browser
+														.getTitle()
+														.then(
+															function(
+																title) {
 
-																					browser
-																							.switchTo()
-																							.window(
-																									handles[1]);
-
-																					browser
-																							.getTitle()
-																							.then(
-																									function(
-																											title) {
-
-																										console
-																												.log(title
-																														+ " title of the page after switching");
-																									})
-
-																					browser
-																							.switchTo()
-																							.window(
-																									handles[0]);
-
-																					browser
-																							.getTitle()
-																							.then(
-																									function(
-																											title) {
-
-																										console
-																												.log(title
-																														+ " title of the page at 3rd time ");
-																									})
-
-																				})
-
+																console
+																	.log(title
+																		+ " title of the page after switching");
 															})
 
-										})
+													browser
+														.switchTo()
+														.window(
+															handles[0]);
 
-					})
-		})
+													browser
+														.getTitle()
+														.then(
+															function(
+																title) {
+
+																console
+																	.log(title
+																		+ " title of the page at 3rd time ");
+															})
+
+												})
+
+									})
+
+						})
+
+			})
+	})
