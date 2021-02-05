164f06f2b922a9eb6dd98cfc1eec92abd37afe7c
harih <harih@HariHRPS2>
2021-02-05 00:41:14 -0600

    no comment

--- workspace-js/ProtractorTutorial/WindowHandles.js
+++ workspace-js/ProtractorTutorial/WindowHandles.js
@@ -15,62 +15,34 @@ describe(
 				browser.actions().mouseMove(element(by.model("locationQuery")).sendKeys("london")).perform();
 				browser.actions().sendKeys(protractor.Key.ARROW_DOWN).perform();
 				browser.actions().sendKeys(protractor.Key.ENTER).perform().then(function() {
-				browser.sleep(2000);
+					browser.sleep(2000);
 
-				expect(element.all(by.css("div[ng-mouseover*='onSearchResultOver']")).count()).toEqual(7);
+					expect(element.all(by.css("div[ng-mouseover*='onSearchResultOver']")).count()).toEqual(7);
 
-				element.all(by.css("div[ng-mouseover*='onSearchResultOver']")).get(0).click();
-				element(by.css("a[ng-href*='London/River Island']")).click().then(function() {browser.getTitle().then(function(
-													title) {
+					element.all(by.css("div[ng-mouseover*='onSearchResultOver']")).get(0).click();
+					element(by.css("a[ng-href*='London/River Island']")).click().then(function() {
+						browser.getTitle().then(function(title) {
 
-													console
-														.log(title
-															+ " title of the page before switching");
-												})
-
-										browser
-											.getAllWindowHandles()
-											.then(
-												function(
-													handles) {
-
-													browser
-														.switchTo()
-														.window(
-															handles[1]);
-
-													browser
-														.getTitle()
-														.then(
-															function(
-																title) {
-
-																console
-																	.log(title
-																		+ " title of the page after switching");
-															})
-
-													browser
-														.switchTo()
-														.window(
-															handles[0]);
-
-													browser
-														.getTitle()
-														.then(
-															function(
-																title) {
-
-																console
-																	.log(title
-																		+ " title of the page at 3rd time ");
-															})
-
-												})
-
-									})
+							console.log(title + " title of the page before switching");
 
 						})
+
+						browser.getAllWindowHandles().then(function(handles) {
+							browser.switchTo().window(handles[1]);
+							browser.getTitle().then(function(title) {
+								console.log(title + " title of the page after switching");
+							})
+
+							browser.switchTo().window(handles[0]);
+							browser.getTitle().then(function(title) {
+								console.log(title + " title of the page at 3rd time ");
+							})
+
+						})
+
+					})
+
+				})
 
 			})
 	})
